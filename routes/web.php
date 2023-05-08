ba38a97b5389ba12e500877e835d302ec9443d2b
MotasemZiad <motasemabunima@gmail.com>
2023-05-08 16:13:08 +0300

    getting started with routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view("about");
+});
+
+Route::get("/contact", function () {
+    return view("contact");
+});
+
+Route::view("/about", "about", [
+    "page_name" => "Laravel | About",
+    'page_description' => "About Us Description Fugiat aute incididunt fugiat cupidatat nisi exercitation nostrud excepteur occaecat. Id cillum sunt sunt laboris adipisicing minim dolore. Et irure quis ea irure.",
+]);
+
+Route::view("/contact", "contact", [
+    "page_name" => "Laravel | Contact",
+    'page_description' => "Contact Us Description Exercitation incididunt nisi aute culpa dolor consequat qui elit aute aliqua ex id sint minim. Eiusmod id eiusmod occaecat ut proident dolor nulla pariatur ea anim exercitation. Anim irure qui et et cupidatat incididunt occaecat labore dolore nulla ipsum nisi veniam quis. Sint eiusmod reprehenderit esse dolore. Labore eiusmod occaecat qui voluptate. Reprehenderit elit eiusmod nulla mollit. Nostrud consectetur reprehenderit ullamco enim ea sit in esse id.",
+]);
